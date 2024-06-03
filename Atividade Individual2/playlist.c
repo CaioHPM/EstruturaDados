c5e2c9fbcb82cd00b30599548b31a9877b334491
Caio Henrique <142283179+CaioHPM@users.noreply.github.com>
2024-06-03 01:59:39 +0000

    criação da struct e lista

--- Atividade Individual2/playlist.c
+++ Atividade Individual2/playlist.c
@@ -1,1 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+typedef struct Musica{
+    char artista[100];
+    char nomeMusica[100];
+    struct Musica* proximo;
+    struct Musica* anterior;
+
+}Musica;
+
+void criarLista(Musica *playlist){
+    if (playlist == NULL)
+    {
+        
+    }
+    
+
+}
+
+int main(){
+
+
+
+
+
+
+    return 0;
+}
