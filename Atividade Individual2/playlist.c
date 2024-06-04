16b574e0c00c54776116b16b3a73be1d84d39791
Caio Henrique <142283179+CaioHPM@users.noreply.github.com>
2024-06-04 14:49:18 +0000

    teste 1

--- Atividade Individual2/playlist.c
+++ Atividade Individual2/playlist.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 typedef struct Musica{
     char artista[100];
@@ -9,17 +10,47 @@ typedef struct Musica{
 
 }Musica;
 
-void criarLista(Musica *playlist){
-    if (playlist == NULL)
+void criarLista(Musica *playlist[]){
+    char linha[300];
+    char *divisor;  
+    int numeroMusica = 0;
+    FILE *arq = fopen("musicas.txt", "r");
+
+    if (arq == NULL)
     {
-        
+        printf("Erro ao abrir o arquivo.\n");
+        exit(1);
     }
     
-
+    while ( fgets(linha, 300, arq) != NULL)     
+    {
+        divisor = strtok(linha, ";");
+        if (divisor)
+        {
+            strcpy(playlist[numeroMusica]->artista, linha);
+            divisor = strtok(linha, "\n");   
+            if (divisor)
+            {
+                strcpy(playlist[numeroMusica]->nomeMusica, linha);
+            }
+        }
+        numeroMusica += 1;
+    }
+    fclose(arq);
 }
 
 int main(){
+    Musica *playlist[50];
 
+    
+    criarLista(playlist);
+
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%s\n", playlist[i]->artista);
+    }
+    
 
 
 
